7f24ebf5b79c3835afdbd1bb14f283e64f22685e
Shams Arefin <arefin.semicolonit@gmail.com>
2022-10-19 17:56:18 +0600

    first commit

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,20 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { typeOrmConfig } from './config/typeorm.config';
 import { User } from './user.entity';
 
 @Module({
   imports: [
-      TypeOrmModule.forRoot({
-        type: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        username: 'postgres',
-        password: '1001',
-        database: 'react-nestjs-project',
-        entities: [User],
-        synchronize: true,
-      }),TypeOrmModule.forFeature([User])
+      TypeOrmModule.forRoot(typeOrmConfig),
+      TypeOrmModule.forFeature([User])
   ],
   controllers: [AppController],
   providers: [AppService],
